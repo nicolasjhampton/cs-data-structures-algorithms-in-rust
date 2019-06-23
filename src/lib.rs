598f7274fc6ae89739a5639e04e01ac4692d7aae
Nic Hampton <nicolasjhampton@gmail.com>
2019-06-22 22:52:26 -0700

    correctly implemented insert method

--- src/lib.rs
+++ src/lib.rs
@@ -7,21 +7,32 @@ pub struct LinkedListNode {
 }
 
 impl LinkedListNode {
+    pub fn new(value: &str) -> LinkedListNode {
+        LinkedListNode {
+            value: String::from(value),
+            next: None
+        }
+    }
+
     pub fn get_next(&self) -> Option<Rc<RefCell<LinkedListNode>>> {
         match &self.next {
-            Some(node) => Some(node.clone()),
+            Some(node) => Some(Rc::clone(&node)),
             None => None
         }
     }
 
-    pub fn set_next(&mut self, next_node: RefCell<LinkedListNode>) {
-        self.next = Some(Rc::new(next_node));
+    pub fn set_next(&mut self, next_node: Rc<RefCell<LinkedListNode>>) {
+        self.next = Some(Rc::clone(&next_node));
     }
 
-    pub fn insert(&mut self, new_node: &mut LinkedListNode) {
-        if let Some(next_node) = self.get_next() {
-            new_node.next = Some(next_node.clone());
+    pub fn insert(&mut self, new_node: RefCell<LinkedListNode>) {
+        match self.get_next() {
+            Some(next_node) => {
+                new_node.borrow_mut().set_next(Rc::clone(&next_node));
+            },
+            None => ()
         }
+        self.set_next(Rc::new(new_node));
     }
 
     pub fn get_value(&self) -> String {
@@ -38,13 +49,40 @@ mod tests {
     fn create_node() {
         let node = LinkedListNode {
             value: String::from("tonia"),
-            next: Some(Rc::new(RefCell::new(LinkedListNode {
-                value: String::from("nic"),
-                next: None
-            })))
+            next: None
         };
 
         assert_eq!("tonia", node.value);
+    }
+
+    #[test]
+    fn new_creates_node() {
+        let node = LinkedListNode::new("tonia");
+        assert_eq!("tonia", node.value);
+    }
+
+    #[test]
+    fn insert_adds_node_to_end_of_list() {
+        let mut node = LinkedListNode::new("tonia");
+        node.insert(RefCell::new(LinkedListNode::new("nic")));
+        if let Some(next) = node.get_next() {
+            assert_eq!("nic", next.borrow().get_value())
+        }
+    }
+
+    #[test]
+    fn insert_adds_node_to_middle_of_list() {
+        let mut node = LinkedListNode::new("tonia");
+        node.insert(RefCell::new(LinkedListNode::new("nic")));
+        node.insert(RefCell::new(LinkedListNode::new("bill")));
+
+        let second_node = Rc::clone(&node.get_next().expect(""));
+
+        assert_eq!(*Rc::clone(&second_node).borrow().get_value(), "bill".to_string());
+
+        let third_node = Rc::clone(&second_node).borrow().get_next().expect("");
+
+        assert_eq!(*Rc::clone(&third_node).borrow().get_value(), "nic".to_string());
     }
 
     #[test]
