eae391f4c61cf4f92634fe10269eae6e9c2a56ef
Nic Hampton <nicolasjhampton@gmail.com>
2019-06-26 23:15:22 -0700

    started tree

---
+++ src/trees/binary.rs
@@ -0,0 +1,35 @@
+use std::{ cell::RefCell, rc::{ Rc, Weak }};
+
+use super::nodes::binary_tree::Node;
+
+struct BinaryTree {
+    root: Rc<RefCell<Node>>
+}
+
+impl BinaryTree {
+    fn new(initial_data: i32) -> BinaryTree {
+        BinaryTree {
+            root: Rc::new(RefCell::new(Node::new(initial_data)))
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_tree() {
+        let tree = BinaryTree {
+            root: Rc::new(RefCell::new(Node::new(0)))
+        };
+
+        assert_eq!(tree.root.borrow().data, 0);
+    }
+
+    #[test]
+    fn new_creates_tree() {
+        let tree = BinaryTree::new(1);
+        assert_eq!(tree.root.borrow().data, 1);
+    }
+}
