e18b4be893dcfac4cdc67f6484418d040d66c2d9
Nic Hampton <nicolasjhampton@gmail.com>
2019-06-26 22:29:16 -0700

    starting binary tree, node created

---
+++ src/trees/nodes/binary_tree.rs
@@ -0,0 +1,42 @@
+use std::{ cell::RefCell, rc::{ Rc, Weak } };
+
+pub struct Node {
+    pub data: i32,
+    pub parent: Option<Weak<RefCell<Node>>>,
+    pub left: Option<Rc<RefCell<Node>>>,
+    pub right: Option<Rc<RefCell<Node>>>
+}
+
+impl Node {
+    pub fn new(data: i32) -> Node {
+        Node {
+            data,
+            parent: None,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn create_node() {
+        let node = Node {
+            data: 0,
+            parent: None,
+            left: None,
+            right: None,
+        };
+
+        assert_eq!(0, node.data);
+    }
+
+    #[test]
+    fn new_creates_node() {
+        let node = Node::new(1);
+        assert_eq!(1, node.data);
+    }
+}
